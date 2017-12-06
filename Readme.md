bf7957fbc3aa00a9db619105399fb444dbefaed1
Tarek Becker <tarek@becker.ly>
2017-12-06 10:23:40 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Multi-tenancy AWS SDk
+
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
+
+Wrapper for the AWS SDK to support multi-tenancy.
+
+Right now only DynamoDB is implemented
---
+++ index.js
@@ -0,0 +1,52 @@
+const Promise = require('bluebird')
+const AWS = require('aws-sdk')
+
+AWS.config.setPromisesDependency(Promise)
+const sts = new AWS.STS({ apiVersion: '2011-06-15' })
+
+const duration = 3600
+
+function DDBFactory (region) {
+  this.region = region || 'eu-central-1'
+  this.ddbcc = {}
+}
+
+DDBFactory.prototype.get = function get (tenant) {
+  const now = new Date().getTime()
+
+  // check if credentials do not exist or are expired
+  if (!this.ddbcc[tenant] ||
+    (this.ddbcc[tenant].acquired + duration < now - (duration / 10))) {
+    const params = {
+      RoleArn: `arn:aws:iam::947257169342:role/tenant-${tenant}`,
+      RoleSessionName: `${tenant}-${now}`,
+      DurationSeconds: duration
+    }
+
+    this.ddbcc[tenant] = {
+      acquired: now,
+      promise: sts
+        .assumeRole(params)
+        .promise()
+        .then((data) => {
+          const {
+            Credentials: {
+              AccessKeyId,
+              SecretAccessKey,
+              SessionToken
+            }
+          } = data
+          return new AWS.DynamoDB({
+            accessKeyId: AccessKeyId,
+            secretAccessKey: SecretAccessKey,
+            sessionToken: SessionToken,
+            apiVersion: '2012-08-10',
+            region: this.region
+          })
+        })
+    }
+  }
+  return this.ddbcc[tenant].promise
+}
+
+module.exports = DDBFactory
