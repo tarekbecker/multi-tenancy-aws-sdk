f16de9734f70deae7d5c2a4cd33006a44e3dd9b9
Tarek Becker <tarek@becker.ly>
2018-01-26 13:45:29 +0100

    Update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Promise = require('bluebird')
 const AWS = require('aws-sdk')
+const lambda = require('lambda-helper')
 
 AWS.config.setPromisesDependency(Promise)
 const sts = new AWS.STS({ apiVersion: '2011-06-15' })
@@ -12,43 +13,47 @@ function DDBFactory (region) {
 }
 
 DDBFactory.prototype.get = function get (tenant) {
-  const now = new Date().getTime()
+  return lambda.tenantInfo()
+    .then(config => config[tenant])
+    .then(tenantinfo => {
+      const now = new Date().getTime()
 
-  // check if credentials do not exist or are expired
-  if (!this.ddbcc[tenant] ||
-    (this.ddbcc[tenant].acquired + duration < now - (duration / 10))) {
-    console.log(`No cached credentials for tenant ${tenant}.`)
+      // check if credentials do not exist or are expired
+      if (!this.ddbcc[tenant] ||
+        (this.ddbcc[tenant].acquired + duration < now - (duration / 10))) {
+        console.log(`No cached credentials for tenant ${tenant}.`)
 
-    const params = {
-      RoleArn: `arn:aws:iam::947257169342:role/tenant-${tenant}`,
-      RoleSessionName: `${tenant}-${now}`,
-      DurationSeconds: duration
-    }
+        const params = {
+          RoleArn: tenantInfo.role,
+          RoleSessionName: `${tenant}-${now}`,
+          DurationSeconds: duration
+        }
 
-    this.ddbcc[tenant] = {
-      acquired: now,
-      promise: sts
-        .assumeRole(params)
-        .promise()
-        .then((data) => {
-          const {
-            Credentials: {
-              AccessKeyId,
-              SecretAccessKey,
-              SessionToken
-            }
-          } = data
-          return new AWS.DynamoDB({
-            accessKeyId: AccessKeyId,
-            secretAccessKey: SecretAccessKey,
-            sessionToken: SessionToken,
-            apiVersion: '2012-08-10',
-            region: this.region
-          })
-        })
-    }
-  }
-  return this.ddbcc[tenant].promise
+        this.ddbcc[tenant] = {
+          acquired: now,
+          promise: sts
+            .assumeRole(params)
+            .promise()
+            .then((data) => {
+              const {
+                Credentials: {
+                  AccessKeyId,
+                  SecretAccessKey,
+                  SessionToken
+                }
+              } = data
+              return new AWS.DynamoDB({
+                accessKeyId: AccessKeyId,
+                secretAccessKey: SecretAccessKey,
+                sessionToken: SessionToken,
+                apiVersion: '2012-08-10',
+                region: this.region
+              })
+            })
+        }
+      }
+      return this.ddbcc[tenant].promise
+    })
 }
 
 module.exports = DDBFactory
